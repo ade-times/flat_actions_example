ad3688b8556bcd9ccfedcf073a4cd631c9c1d0bf
Ademola Bello <ademola.bello@news.co.uk>
2021-12-31 01:30:59 +0000

    add clean

---
+++ clean.R
@@ -0,0 +1,17 @@
+# Load libraries
+library(dplyr)
+library(stringr)
+
+# Read in data, with the same name that we specified in `flat.yml`
+raw_data <- readxl::read_excel("./raw.xlsx")
+
+# All the processing!
+clean_data <- raw_data %>%
+  mutate(Zipcode = as.character(Zipcode),
+         Year = lubridate::year(Date),
+         Sex = ifelse(is.na(Sex), 'Unknown', Sex))
+
+### Additional processing goes here...
+
+# Output data
+readr::write_csv(clean_data, "./output.csv")
